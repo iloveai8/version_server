c3d921ca912d43d4273e4c2f181093fa2fb25c99
chenhu <chenhu@fotoable.com>
2022-03-11 10:38:05 +0800

    ch:test dev path

--- internal/api/server.go
+++ internal/api/server.go
@@ -36,8 +36,8 @@ func StartServer(web *config.WebConfig) {
 
 	router.GET("/heartbeat", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"vsn": os.Getenv("vsn"),
-			"env":os.Getenv("env"),
+			"vsn":  os.Getenv("vsn"),
+			"env":  os.Getenv("env"),
 			"time": time.Now(),
 		})
 	})
@@ -45,13 +45,12 @@ func StartServer(web *config.WebConfig) {
 	vsnHandler := handler.NewVsnHandler()
 	vsn := router.Group("v1")
 	{
-		vsn.GET("/vsn/list", vsnHandler.GetAll) //获取所有用户
-		vsn.GET("/vsn", vsnHandler.Get)         //根据id获取用户
-		vsn.POST("/vsn", vsnHandler.Insert)     //保存新用户
-		vsn.PUT("/vsn", vsnHandler.Update)      //根据id更新用户
-		vsn.DELETE("/vsn", vsnHandler.Delete)   //根据id删除用户
-
-		vsn.POST("/vsn/gmConf", vsnHandler.InsertGmConf) //保存GM开关配置
+		vsn.GET("vsn/list", vsnHandler.GetAll)          //获取所有用户
+		vsn.GET("vsn", vsnHandler.Get)                  //根据id获取用户
+		vsn.POST("vsn", vsnHandler.Insert)              //保存新用户
+		vsn.PUT("vsn", vsnHandler.Update)               //根据id更新用户
+		vsn.DELETE("vsn", vsnHandler.Delete)            //根据id删除用户
+		vsn.POST("vsn/gmConf", vsnHandler.InsertGmConf) //保存GM开关配置
 	}
 	if err := router.Run(addr); err != nil {
 		logger.Logger.Info("service start fail.", err)
