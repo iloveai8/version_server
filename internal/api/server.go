bde69c32e2ec21a86c9c46ed37ddfe5d52103c00
chenhu <chenhu@fotoable.com>
2022-03-15 21:24:50 +0800

    ch:modify server.go

--- internal/api/server.go
+++ internal/api/server.go
@@ -30,36 +30,47 @@ func StartServer(web *config.WebConfig) {
 	router.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "Not router")
 	})
-	router.GET("/:env/favicon.ico", func(c *gin.Context) {
-		c.String(http.StatusOK, "ok")
-	})
 	router.GET("/favicon.ico", func(c *gin.Context) {
 		c.String(http.StatusOK, "ok")
 	})
+	router.GET("/:env/favicon.ico", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
 	router.GET("/heartbeat", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"vsn":  os.Getenv("vsn"),
-			"env":  os.Getenv("env"),
+			"vsn": os.Getenv("vsn"),
+			"env":os.Getenv("env"),
 			"time": time.Now(),
 		})
 	})
 	router.GET("/:env/heartbeat", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"vsn":  os.Getenv("vsn"),
-			"env":  os.Getenv("env"),
+			"vsn": os.Getenv("vsn"),
+			"env":os.Getenv("env"),
 			"time": time.Now(),
 		})
 	})
-
 	vsnHandler := handler.NewVsnHandler()
-	vsn := router.Group("/:env/v1").Group("/v1")
+	vsn := router.Group("/v1")
 	{
-		vsn.GET("vsn/list", vsnHandler.GetAll)          //获取所有用户
-		vsn.GET("vsn", vsnHandler.Get)                  //根据id获取用户
-		vsn.POST("vsn", vsnHandler.Insert)              //保存新用户
-		vsn.PUT("vsn", vsnHandler.Update)               //根据id更新用户
-		vsn.DELETE("vsn", vsnHandler.Delete)            //根据id删除用户
-		vsn.POST("vsn/gmConf", vsnHandler.InsertGmConf) //保存GM开关配置
+		vsn.GET("/vsn/list", vsnHandler.GetAll) //获取所有用户
+		vsn.GET("/vsn", vsnHandler.Get)         //根据id获取用户
+		vsn.POST("/vsn", vsnHandler.Insert)     //保存新用户
+		vsn.PUT("/vsn", vsnHandler.Update)      //根据id更新用户
+		vsn.DELETE("/vsn", vsnHandler.Delete)   //根据id删除用户
+
+		vsn.POST("/vsn/gmConf", vsnHandler.InsertGmConf) //保存GM开关配置
+	}
+	vsn1 := router.Group("/:env/v1")
+	{
+		vsn1.GET("/vsn/list", vsnHandler.GetAll) //获取所有用户
+		vsn1.GET("/vsn", vsnHandler.Get)         //根据id获取用户
+		vsn1.POST("/vsn", vsnHandler.Insert)     //保存新用户
+		vsn1.PUT("/vsn", vsnHandler.Update)      //根据id更新用户
+		vsn1.DELETE("/vsn", vsnHandler.Delete)   //根据id删除用户
+
+		vsn1.POST("/vsn/gmConf", vsnHandler.InsertGmConf) //保存GM开关配置
 	}
 	if err := router.Run(addr); err != nil {
 		logger.Logger.Info("service start fail.", err)
