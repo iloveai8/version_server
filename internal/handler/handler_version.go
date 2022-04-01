feb1b5842275ccf18a8da6c83724c86f3a643bc4
chenhu <chenhu@fotoable.com>
2022-04-01 12:57:24 +0800

    ch:add 公司出口ip

--- internal/handler/handler_version.go
+++ internal/handler/handler_version.go
@@ -17,24 +17,16 @@ const (
 	CacheGMConfKey string = "gm.conf."
 )
 
-var IPMap = map[string]int{
-	"1.202.246.18":    1,
-	"1.202.246.19":    1,
-	"1.202.246.20":    1,
-	"1.202.246.21":    1,
-	"1.202.246.30":    1,
-	"103.85.165.146":  1,
-	"106.120.91.66":   1,
-	"106.120.91.67":   1,
-	"106.120.91.68":   1,
-	"40.83.97.197":    1,
-	"47.75.45.195":    1,
-	"47.75.59.239":    1,
-	"49.51.197.144":   1,
-	"119.81.164.4":    1,
-	"129.226.60.247":  1,
-	"129.226.189.243": 1,
-	"43.154.154.31":   1,
+var IPArray = [...]string{
+	"103.85.165.146/29",
+	"106.120.91.66/28",
+	"106.120.91.67/28",
+	"106.120.91.68/28",
+	"1.202.246.18/28",
+	"1.202.246.19/28",
+	"1.202.246.20/28",
+	"1.202.246.21/28",
+	"1.202.246.30/28",
 }
 
 type VsnHandler struct {
@@ -91,18 +83,18 @@ func (vh VsnHandler) Get(c *gin.Context) {
 	isGM := false
 	if gmConf.GMEnable {
 		ip := c.ClientIP()
-		logger.Logger.Warnf(" ==>gm enable:%v ip:~%v", gmConf.GMEnable, ip)
-		if matchIp(ip) {
-			logger.Logger.Warnf(" ==>client ip:%v is in inner white list:%v", ip, IPMap)
-			vsnInfo.SrvUrl = gmConf.GMSrvUrl
-			vsnInfo.ResUrl = gmConf.GMResUrl
-			isGM = true
-		} else if _, ok := IPMap[ip]; ok {
-			logger.Logger.Warnf(" ==>client ip:%v is in out company white list:%v", ip, IPMap)
+		if MatchIp(ip) {
+			logger.Logger.Warnf(" ==>gm enable:%v client ip:%v is in inner", gmConf.GMEnable, ip)
 			vsnInfo.SrvUrl = gmConf.GMSrvUrl
 			vsnInfo.ResUrl = gmConf.GMResUrl
 			isGM = true
 		}
+		//else if _, ok := IPMap[ip]; ok {
+		//	logger.Logger.Warnf(" ==>client ip:%v is in out company white list:%v", ip, IPMap)
+		//	vsnInfo.SrvUrl = gmConf.GMSrvUrl
+		//	vsnInfo.ResUrl = gmConf.GMResUrl
+		//	isGM = true
+		//}
 	}
 	logger.Logger.Infof(" ==>gmConf:%v vsnInfo:%v", gmConf, vsnInfo)
 	reply := map[string]interface{}{
@@ -200,15 +192,24 @@ func (vh VsnHandler) InsertGmConf(c *gin.Context) {
 	c.JSON(http.StatusOK, respone.Success(gmConfInfo))
 }
 
-func matchIp(IP string) bool {
-	//255.0.0.0
-	//测试网段建议连接公司无线信号fotoable-WIFI6
-	//该信号内网地址：10.0.132.0/23
-	network := "10.0.0.0/8"
-	_, subnet, _ := net.ParseCIDR(network)
-	if subnet.Contains(net.ParseIP(IP)) {
-		fmt.Println("addr:", IP, "in subnet")
-		return true
+//"103.85.165.146/29",
+
+//"106.120.91.66/28",
+//"106.120.91.67/28",
+//"106.120.91.68/28",
+
+//"1.202.246.18/28",
+//"1.202.246.19/28",
+//"1.202.246.20/28",
+//"1.202.246.21/28",
+//"1.202.246.30/28",
+func MatchIp(IP string) bool {
+	for _, network := range IPArray {
+		_, subnet, _ := net.ParseCIDR(network)
+		if subnet.Contains(net.ParseIP(IP)) {
+			fmt.Println("addr:", IP, "in subnet:v%", subnet)
+			return true
+		}
 	}
 	return false
 }
